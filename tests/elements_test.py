f624a02261788efc2b433b6fe9eeac36d7211a6f
a.sinkov <a.sinkov@rambler-co.ru>
2023-04-27 00:18:58 +0300

    fixed base_page script element update elements tests add new test checkboxes

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElement:
@@ -16,3 +16,15 @@ class TestElement:
             assert email == output_email, "the email doesn't match"
             assert current_address == output_cur_address, "the current address doesn't match"
             assert permanent_address == output_perm_address, "the permanent address doesn't match"
+
+    class TestCheckBox:
+        def test_check_box(self, browser):
+            check_box_page = CheckBoxPage(browser, "https://demoqa.com/checkbox")
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            print(output_result)
+            print(input_checkbox)
+            assert input_checkbox == output_result, "checkboxes haven't been selected"
