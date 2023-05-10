ef4925eaa2c28e60ed883f598aa42247570b2265
a.sinkov <a.sinkov@rambler-co.ru>
2023-05-10 17:01:19 +0300

    update elements tests fixes added class: -TestUploadAndDownload added tests: -test_upload_file -test_download_file

--- generator/generator.py
+++ generator/generator.py
@@ -19,3 +19,11 @@ def generated_person():
         current_address=faker_ru.address(),
         permanent_address=faker_ru.address(),
     )
+
+
+def generated_file():
+    path = f"/Users/an.sinkov/PycharmProjects/pet_project01/{random.randint(0, 999)}.txt"
+    file = open(path, 'w+')
+    file.write(f'Hello world{random.randint(0, 999)}')
+    file.close()
+    return file.name, path
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,7 +1,8 @@
 import random
 import time
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, LinksPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, LinksPage, \
+    TestUploadAndDownloadPage
 
 
 class TestElement:
@@ -119,3 +120,18 @@ class TestElement:
             links_page.open()
             response_code = links_page.check_broken_link('https://demoqa.com/bad-request')
             assert response_code == 400, "the link works or the status code in son 400"
+
+    class TestUploadAndDownload:
+
+        # to do change path name for correct work
+        def test_upload_file(self, browser):
+            upload_download_page = TestUploadAndDownloadPage(browser, "https://demoqa.com/upload-download")
+            upload_download_page.open()
+            file_name, result = upload_download_page.upload_file()
+            assert file_name == result, "the file has not been uploaded"
+
+        def test_download_file(self, browser):
+            upload_download_page = TestUploadAndDownloadPage(browser, "https://demoqa.com/upload-download")
+            upload_download_page.open()
+            check = upload_download_page.download_file()
+            assert check is True, "the file has not been downloaded"
