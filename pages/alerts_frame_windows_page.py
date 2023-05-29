685ed813d69f49483491b967dcad6b90802ca568
a.sinkov <a.sinkov@rambler-co.ru>
2023-05-29 23:53:29 +0300

    added class: TestAlertsPage added tests: test_see_alert test_alert_appear_5_sec test_confirm_alert test_prompt_alert

--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -1,9 +1,11 @@
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+import random
+import time
+
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertsPageLocators
 from pages.base_page import BasePage
 
 
 class BrowserWindowsPage(BasePage):
-
     locators = BrowserWindowsPageLocators()
 
     def check_opened_new_tab(self):
@@ -17,3 +19,36 @@ class BrowserWindowsPage(BasePage):
         self.driver.switch_to.window(self.driver.window_handles[1])
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
+
+
+class AlertsPage(BasePage):
+    locators = AlertsPageLocators()
+
+    def check_see_alert(self):
+        self.element_is_visible(self.locators.SEE_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        # print(alert_window.text)
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.element_is_visible(self.locators.APPEAR_ALERT_BUTTON).click()
+        time.sleep(6)
+        alert_window = self.driver.switch_to.alert
+        # print(alert_window.text)
+        return alert_window.text
+
+    def check_confirm_alert(self):
+        self.element_is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.CONFIRM_RESULT).text
+        return text_result
+
+    def check_prompt_alert(self):
+        text = f"autotest{random.randint(0, 999)}"
+        self.element_is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.PROMPT_RESULT).text
+        return text, text_result
